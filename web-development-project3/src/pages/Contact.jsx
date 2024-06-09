b57daf1330e2c1463403be01bb747a3bba5126ae
djshiye <dreamfantom16@gmail.com>
2024-06-09 13:48:25 +0200

    Project3 Styling

--- web-development-project3/src/pages/Contact.jsx
+++ web-development-project3/src/pages/Contact.jsx
@@ -49,63 +49,68 @@ const Contact = () => {
   }, []);
 
   return (
-    <Container sx={{ marginTop: 8 }}>
-      <Typography variant="h4" gutterBottom align="center">Contact Us</Typography>
-      <form noValidate autoComplete="on" onSubmit={handleSubmit}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Name"
-              name="name"
-              fullWidth
-              margin="normal"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              error={!!errors.name}
-              helperText={errors.name}
-            />
+    <div style={{ background: "url('https://images.unsplash.com/photo-1600762516498-761775b86af7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')", backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh', display: 'flex', alignItems: 'center' }}>
+      <Container>
+        <Typography variant="h4" gutterBottom align="center" style={{ marginBottom: 20, color: 'white' }}>Contact Us</Typography>
+        <form noValidate autoComplete="on" onSubmit={handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Name"
+                name="name"
+                fullWidth
+                margin="normal"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                error={!!errors.name}
+                helperText={errors.name}
+                InputProps={{ style: { color: 'white' } }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Email"
+                name="email"
+                type="email"
+                fullWidth
+                margin="normal"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={!!errors.email}
+                helperText={errors.email}
+                InputProps={{ style: { color: 'white' } }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Message"
+                name="message"
+                fullWidth
+                multiline
+                rows={4}
+                margin="normal"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                error={!!errors.message}
+                helperText={errors.message}
+                InputProps={{ style: { color: 'white' } }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {/* Conditional rendering of message sent confirmation */}
+              {messageSent && (
+                <Typography variant="body1" align="center" sx={{ color: 'green', marginTop: 1 }}>
+                  Message sent successfully!
+                </Typography>
+              )}
+              <Box display="flex" justifyContent="center">
+                <Button type="submit" variant="contained" color="primary">Submit</Button>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Email"
-              name="email"
-              type="email"
-              fullWidth
-              margin="normal"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              error={!!errors.email}
-              helperText={errors.email}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Message"
-              name="message"
-              fullWidth
-              multiline
-              rows={4}
-              margin="normal"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              error={!!errors.message}
-              helperText={errors.message}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            {/* Conditional rendering of message sent confirmation */}
-            {messageSent && (
-              <Typography variant="body1" align="center" sx={{ color: 'green', marginTop: 1 }}>
-                Message sent successfully!
-              </Typography>
-            )}
-            <Box display="flex" justifyContent="center">
-              <Button type="submit" variant="contained" color="primary">Submit</Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
+        </form>
+      </Container>
+    </div>
   );
 };
 
